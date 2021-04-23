0f2a92f2df962708d089d7621765a18dad63bd6a
Jelle Wie¿ <1545223+jellewie@users.noreply.github.com>
2021-04-23 11:17:16 +0200

    Fixed spacing code notations

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,18 +1,17 @@
 /*
   Program written by JelleWho https://github.com/jellewie
 */
-
-//#define StableAnalog_AverageAmount 16                 //On how much points to take the average from (default=16)
-//#define StableAnalog_AnalogResolution 8               //Howmany bits an analog read is (default=8 same as default Arduino boards)
+//#define StableAnalog_AverageAmount 16                         //On how much points to take the average from (default=16)
+//#define StableAnalog_AnalogResolution 8                       //Howmany bits an analog read is (default=8 same as default Arduino boards)
 
 #include "StableAnalog/StableAnalog.h"
 
-StableAnalog PotMeterA = StableAnalog(1);               //Declare that we want to stabilize this input
-StableAnalog PotMeterB = StableAnalog(15);              //Declare some variable (in this case PotMeterB) to be at some pin (in this case 15)
+StableAnalog PotMeterA = StableAnalog(1);                       //Declare that we want to stabilize this input
+StableAnalog PotMeterB = StableAnalog(15);                      //Declare some variable (in this case PotMeterB) to be at some pin (in this case 15)
 
 void setup() {
   Serial.begin(115200);
-  analogReadResolution(StableAnalog_AnalogResolution);  //Set StableAnalog_AnalogResolution
+  analogReadResolution(StableAnalog_AnalogResolution);          //Set StableAnalog_AnalogResolution
 }
 
 void loop() {
@@ -26,12 +25,12 @@ void loop() {
   //SkipFirst    Skip the first x of measurements, this is so we have proper averages, else if will climb up from 0
   //Mode         0=analogRead(default)  1=touchRead
   
-  if (A.Changed) {    //If the value has changed (0=FALSE=not changed, and 1>=TRUE=amount of changed)
+  if (A.Changed) {                                              //If the value has changed (0=FALSE=not changed, and 1>=TRUE=amount of changed)
     Serial.println("Analog read A changed to " + String(A.Value) + " which is a change of " + String(A.Changed));
   }
-
+  
   POT B = PotMeterA.ReadStable(2, 3, 0);
-  if (B.Changed) {    //If the value has changed (0=FALSE=not changed, and 1>=TRUE=amount of changed)
+  if (B.Changed) {                                              //If the value has changed (0=FALSE=not changed, and 1>=TRUE=amount of changed)
     Serial.println("Analog read B changed to " + String(B.Value) + " which is a change of " + String(B.Changed));
   }
 
